c8f2c9c501e395af73409ace16c4d2c54801a98a
ethan <172529131@qq.com>
2018-10-10 18:49:34 +0800

    add koa-views

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
-const { htmlTpl, ejsTpl, pugTpl } = require('./template');
-// const ejs = require('ejs');
-const pug = require('pug');
+const views = require('koa-views')
+const { resolve } = require('path')
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8'
-  // ctx.body = ejs.render(ejsTpl, {
-  //   you: '宋冬冬',
-  //   me: '东西系'
-  // })
-  ctx.body = pug.render(pugTpl, {
-    you: '宋冬冬',
-    me: '过江龙'
+  await ctx.render('index', {
+    you: 'pig',
+    me: 'Ethan'
   })
 })
 app.listen(4455, () => {
