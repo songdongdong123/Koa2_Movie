9c482346c039229df1513ce550c0f1491f6ea61c
ethan <172529131@qq.com>
2018-10-16 16:26:33 +0800

    add react

---
+++ src/views/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render () {
+    return (
+      <div className="home">
+        <p>首页</p>
+      </div>
+    )
+  }
+}
---
+++ src/views/movie/detail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Detail extends React.Component {
+  render () {
+    return (
+      <div className="home">
+        <p>详情页</p>
+      </div>
+    )
+  }
+}
